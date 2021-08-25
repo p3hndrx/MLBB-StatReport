7952e96b930e05ee5499a42e4ec6ba8b1b52d1fe
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2021-08-25 19:47:36 -0400

    REWORK

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ import roles
 # endregion
 
 # region ENVIRONMENT
-logging.basicConfig(filename="/tmp/sync-summarygen.log", level=logging.DEBUG,
+logging.basicConfig(filename="/tmp/sync-reportgen.log", level=logging.DEBUG,
                     format="%(asctime)s:%(levelname)s:%(message)s")
 logging.info(f"********************** STARTING REPORT GEN V1")
 print("Starting Report Gen...")
@@ -229,12 +229,19 @@ for l in lang:
                             #print(f"Bad Request: Missing: {jsonfile}")
                             logging.warning(f"Bad Request: Missing: {jsonfile}")
 
+                    #Remove Outliers
+                    print(f"Removing Outliers...")
+                    logging.info(f"Combined CSV: {reportout}/{r}.{m}.{lvl}.csv")
+                    dfx.drop(dfx[dfx['win'] == 100].index, inplace=True)
+                    dfx.drop(dfx[dfx['use'] <= .001].index, inplace=True)
+                    dfx.drop(dfx[dfx['kda'] >= 20].index, inplace=True)
+
                     # TEST OUT TO CSV
-                    print(f"{dfx}")
+                    print(f"Source Table... \n{dfx}")
                     dfx.to_csv(f"{reportout}/{r}.{m}.{lvl}.csv",index=False)
                     print(f"Combined CSV: {reportout}/{r}.{m}.{lvl}.csv")
                     logging.info(f"Combined CSV: {reportout}/{r}.{m}.{lvl}.csv")
-                    input("Press Enter to continue...")
+                    #input("Press Enter to continue...")
     #TEST MAIN OUT
     #print(f"{dfx}")
     #dfx.to_csv(f"{reportout}/master.csv", index=False)
@@ -249,53 +256,51 @@ for l in lang:
                         rslt = getattr(roles, p)
                         dfp = dfx[dfx['name'].isin(rslt)]
                         print(f"{dfp}")
-                        input("Press Enter to continue...")
 
-'''
-                    heroes = dfx['name'].unique()
-                    for hero in heroes:
-                        plt.style.use('dark_background')
+                        #FILTER top 5:
+                        crit = ['win','use','kda']
+                        latest = dfp['runtime'].iloc[-1]
 
-                        # OUTPUT LINECHART
-                        df2 = dfx[dfx['name'] == hero]
-                        df2 = df2[['runtime', 'win', 'use', 'kda']]
-                        print(f"{hero}\n{df2}")
+                        for c in crit:
+                            print(f"Last Date: {latest}; Top: {c}")
+                            rslt_df = dfp[dfp['runtime'] == latest]
+                            rslt_df = rslt_df.sort_values(by=[str(c)]).head(5)
+                            #print(f"{rslt_df}")
+                            #input("Press Enter to continue...")
 
-                        shero = hero.replace("-", "").replace("'", "").replace(".", "").replace(" ", "").lower()
-                        op = f"{outpath}/{r}/{m}/{lvl}/{shero}.png"
+                            #print(f"{rslt_df['name']}")
+                            top = rslt_df['name'].tolist()
+                            print(f"{top}")
+                            input("Press Enter to continue...")
+                            dfc = dfp[dfp['name'].isin(top)]
+                            dfc.sort(by=[str(c)], ascending=[1, 0])
 
-                        # fig, ax = plt.subplots(facecolor='darkslategrey')
-                        fig, axs = plt.subplots(nrows=3, figsize=(7, 5), facecolor='darkslategrey', sharex=True)
-                        for ax, column, color, (min_normal, max_normal) in zip(axs,
-                                                                               ['win', 'use', 'kda'],
-                                                                               ['khaki', 'lightcyan', 'thistle'],
-                                                                               [(10, 90), (0.05, 10), (0.05, 10)]):
-                            df2.plot(x='runtime', xlabel="Date", y=column, ylabel=column, kind='line', marker='o',
-                                     linewidth=2, alpha=.7, color=color, legend=False, ax=ax)
-                            df_abnormal = df2[(df2[column] < min_normal) | (df2[column] > max_normal)]
-                            df_abnormal.plot(x='runtime', xlabel="Date", y=column, ylabel=column, kind='scatter',
-                                             marker='D', color='red', legend=False, zorder=3, ax=ax)
-                        plt.style.use('dark_background')
-                        plt.xticks(rotation=15)
-                        plt.suptitle(f'Historical Data for {hero}\nRegion: {r}, Elo: {lvl}, Mode:{m}', fontsize=12,
+                            #Graph:
+                            print(f"Charting Changes...{r}.{m}.{lvl}.{p}: by {c}")
+                            plt.style.use('dark_background')
+                            op = f"{reportout}/{r}.{m}.{lvl}.{p}.{c}.png"
+                            n = len(pd.unique(dfc['name']))
+
+                            fig, ax = plt.subplots(nrows=n, sharex=True)
+                            #dfp.pivot(index='runtime', columns='name', values='win').plot(subplots=True, layout=(n, 1), figsize=(6, 9), marker='o',linewidth=2)
+                            for i, name in enumerate(dfc['name'].unique(), 0):
+                                df_filtered = dfc[dfc['name'] == name]
+                                ax[i].plot(df_filtered['runtime'], df_filtered[str(c)], marker='o', linewidth=2)
+                                ax[i].set_ylabel(name)
+
+                            plt.style.use('dark_background')
+                            plt.xticks(rotation=15)
+                            plt.suptitle(f'Historical Data for {p}\nRegion: {r}, Elo: {lvl}, Mode:{m}', fontsize=12,
                                      fontname='monospace')
+                            # file output
+                            plt.savefig(op, transparent=False, bbox_inches="tight")
 
-                        if (df2['kda'] > 20).any() or (df2['kda'] < .05).any() or (df2['win'] == 100).any() or (
-                                df2['use'] == 0).any():
-                            print(f"We have an outlier.")
-                            plt.figtext(0, 0.01,
-                                        "*NOTE: A statistically improbable anomaly was detected in the data you have requested.",
-                                        fontsize=9, fontname='monospace', color='black',
-                                        bbox={"facecolor": "yellow", "alpha": 0.5, "pad": 5})
+                            print(f"Combined Image: {op}")
+                            logging.info(f"Combined Image: {op}")
+                            #plt.show()
+                            plt.close('all')
+                            input("Press Enter to continue...")
 
-                        # file output
-                        plt.savefig(op, transparent=False, bbox_inches="tight")
-
-                        print(f"Output Plot: {op}")
-                        logging.info(f"Output Plot: {op}")
-                        # plt.show()
-                        plt.close('all')
-'''
 
 # endregion
 
