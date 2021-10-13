2040c45fade278d404f7d1928d657274efb7f2e1
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2021-10-13 15:42:16 -0400

    Fixed outliers

--- index-baseXmode-box.py
+++ index-baseXmode-box.py
@@ -211,7 +211,7 @@ for l in lang:
                     # dfx.drop(dfx[dfx['win'] == 100].index, inplace=True)
                     # dfx.drop(dfx[dfx['use'] <= .001].index, inplace=True)
                     # dfx.drop(dfx[dfx['kda'] >= 20].index, inplace=True)
-                    # dfx = dfx[dfx.use >= .001]
+                    dfx = dfx[dfx.use <= 10]
                     # dfx = dfx[dfx.win != 100]
                     dfx = dfx[dfx.kda <= 20]
 
