26a35ee34ceec7e0c9e1a6abacd142fb7c1f82c1
p3hndrx <14354635+p3hndrx@users.noreply.github.com>
2021-11-18 20:36:00 -0500

    Updated time range on PROD generators

--- rd.index-baseXall-DEV.py
+++ rd.index-baseXall-DEV.py
@@ -34,7 +34,6 @@ yesterday = y.strftime("%Y%m%d")
 
 # PATHS
 rawpath = "/tmp/RankData.fake"
-#imgsrc = "/root/MLBB-StatReport/heroes"
 imgsrc = "/Users/phunr/PycharmProjects/MLBB-StatReport/heroes"
 reportout = "/tmp/baseXall.rd"
 
@@ -49,10 +48,7 @@ logging.info(f"Crawl Runtimes: {runtimes}")
 
 # DATASETS
 crit = ['win','use','ban']
-#lang = ["en"]
-
 #dtrange = ["Week"]
-#mode = ["All-Modes", "Classic", "Rank", "Brawl"]
 level = ["All", "Legend", "Mythic"]
 prof = ["assassin","marksman","mage","tank","support","fighter"]
 
@@ -94,66 +90,17 @@ print("Compiling Lookup...")
 logging.info("Compiling Lookup")
 
 # Create master table
-runtimes = sorted(runtimes, reverse=False)
+runtimes = sorted(runtimes, reverse=True)
 
 # START THE CRAWLER
 i = 0
 t = 0
 
 for lvl in level:
-    dfx = pd.DataFrame(columns=['runtime','name', 'win', 'use', 'ban', 'wrank', 'urank', 'banrank', 'elo'])
-    for pt in runtimes[:30]:
+    from functions import statstable
+    dfx = statstable(50, rawpath)
 
-        #constructoutput
-        jsonfile = f'{rawpath}/{pt}/{lvl}.json'
-        if os.path.exists(jsonfile):
-            print("Requesting: " + jsonfile)
-            logging.info("Requesting: " + jsonfile)
-
-            ##### BUILD TABLES ####
-            with open(jsonfile) as j:
-                data = json.load(j)
-
-                df = json_normalize(data, ['data', 'data'])
-
-                # convert from strings:
-                df['win'] = list(map(lambda x: x[:-1], df['win'].values))
-                df['use'] = list(map(lambda x: x[:-1], df['use'].values))
-                df['ban'] = list(map(lambda x: x[:-1], df['ban'].values))
-
-                df['win'] = [float(x) for x in df['win'].values]
-                df['use'] = [float(x) for x in df['use'].values]
-                df['ban'] = [float(x) for x in df['ban'].values]
-
-                # add ranking column
-                df = df.sort_values(by=['use'], ascending=False)
-                df['urank'] = range(1, len(df) + 1)
-                df = df.sort_values(by=['win'], ascending=False)
-                df['wrank'] = range(1, len(df) + 1)
-                df = df.sort_values(by=['ban'], ascending=False)
-                df['banrank'] = range(1, len(df) + 1)
-
-                df['runtime'] = f"{pt}"
-                df['runtime'] = df['runtime'].astype('datetime64[ns]')
-                df['elo'] = f"{lvl}"
-
-                dfx = pd.concat([dfx, df], axis=0)
-        else:
-            print(f"Bad Request: Missing: {jsonfile}")
-            logging.warning(f"Bad Request: Missing: {jsonfile}")
-
-    #TEST OUT TO CSV
-    #dfx.to_csv(f"{outpath}/{r}/{m}/{lvl}.csv",index=False)
-    print(f"Combined:{lvl}-\n{dfx}")
-    #logging.info(f"Combined:{lvl}-{m}-{r}\n{dfx}")
-
-
-    #TEST MAIN OUT
-    #print(f"{dfx}")
-    #dfx.to_csv(f"{reportout}/master.csv", index=False)
-    #print(f"Combined CSV: {reportout}/master.csv")
-    #logging.info(f"Combined CSV: {reportout}/master.csv")
-    #input("Press Enter to continue...")
+    dfx = dfx[dfx['elo'].str.contains(lvl)]
 
     fig, ax = plt.subplots(facecolor='darkslategrey')
     plt.style.use('dark_background')
@@ -202,7 +149,10 @@ for lvl in level:
             fontsize=12,
             fontname='monospace')
         plt.legend(loc=2)
-        # ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+        ax.xaxis.set_minor_locator(mdates.DayLocator(interval=1))
+        ax.xaxis.set_minor_formatter(mdates.DateFormatter(''))
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=5))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 
         # LABEL
         print("Generating Labels...")
@@ -216,6 +166,7 @@ for lvl in level:
             # search
             dfh = rslt_df[rslt_df['name'] == hero]
             val = dfh[str(c)].values[0]
+            val = round(val,2)
             xy = (d, val)
 
             fn = get_sample_data(f"{imgsrc}/{shero}.png", asfileobj=False)
